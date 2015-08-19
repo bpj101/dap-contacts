f998d1d16475a339ce27418ff6d3bcf71cdc7675
Paul Julian <bpj101@dameagnesproductions.com>
2015-08-19 13:20:32 -0400

    Added AddForm and functionality

--- app/app.js
+++ app/app.js
@@ -1,12 +1,15 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
+angular.module('dapContacts', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'firebase',
+  'dapContacts.contacts'
 ]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+config(['$routeProvider',
+  function ($routeProvider) {
+    $routeProvider.otherwise({
+      redirectTo: '/contacts'
+    });
+  }
+]);
---
+++ app/contacts/contacts.js
@@ -0,0 +1,95 @@
+'use strict';
+
+angular.module('dapContacts.contacts', ['ngRoute', 'firebase'])
+
+.config(['$routeProvider',
+  function ($routeProvider) {
+    $routeProvider
+      .when('/contacts', {
+        templateUrl: 'contacts/contacts.html',
+        controller: 'ContactsCtrl'
+      });
+  }
+])
+
+//CONTROLLER
+.controller('ContactsCtrl', ['$scope', '$firebaseArray',
+
+  function ($scope, $firebaseArray) {
+    var fbRef = new Firebase('https://dap-contacts.firebaseio.com/contacts');
+
+    // Clear $scope Fields
+    function clearFields() {
+      console.log('Clearing All Fields...');
+
+      $scope.name = '';
+      $scope.email = '';
+      $scope.company = '';
+      $scope.mobile_phone = '';
+      $scope.home_phone = '';
+      $scope.work_phone = '';
+      $scope.street_address = '';
+      $scope.city = '';
+      $scope.state = '';
+      $scope.zipcode = '';
+    }
+
+    //GET CONTACTS
+    $scope.contacts = $firebaseArray(fbRef);
+    // console.log($scope.contacts);
+
+    //SHOW & HIDE ADDFORM
+    $scope.showAddForm = function () {
+      $scope.addFormShow = true;
+    };
+    $scope.hide = function () {
+      $scope.addFormShow = false;
+    };
+    //SUBMIT ADD FORM
+    $scope.addFormSubmit = function () {
+
+      //assign values
+      var name = $scope.name || null;
+      var email = $scope.email || null;
+      var company = $scope.company || null;
+      var mobile_phone = $scope.mobile_phone || null;
+      var home_phone = $scope.home_phone || null;
+      var work_phone = $scope.work_phone || null;
+      var street_address = $scope.street_address || null;
+      var city = $scope.city || null;
+      var state = $scope.state || null;
+      var zipcode = $scope.zipcode || null;
+
+      $scope.contacts.$add({
+        name: name,
+        email: email,
+        company: company,
+        phone: [{
+          mobile: mobile_phone,
+          home: home_phone,
+          work: work_phone
+        }],
+        address: [{
+          street_address: street_address,
+          city: city,
+          state: state,
+          zipcode: zipcode
+        }]
+      })
+        .then(function (fbRef) {
+          var id = fbRef.key();
+          console.log('Added Ccontact with ID: ' + id);
+
+          // Clear Form
+          clearFields();
+
+          // Hide Form
+          $scope.addFormShow = false;
+
+          // Send Message
+          $scope.msg = 'Contact Added';
+        });
+
+    };
+  }
+]);
